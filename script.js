2f85bfdbaf1d6b484745e2cd36d7305da0b3fbe2
Douglas Snodgrass <douglassnodgrass@Mac-mini.lan>
2022-05-18 22:44:56 -0700

    question layout

---
+++ script.js
@@ -0,0 +1,43 @@
+//1.You will have 15 seconds to answer each question
+//2.You can not reselect an answer
+//3.You can not select answe after time runs out
+//4.You cant exit quiz while playing
+//5.You earn points for crrect answers
+
+//API traffic that is entilry internal to you organization is normally called?
+//inbound trafffic
+//north-south traffic
+//internal trafffic correct 
+//eat-wet traffic
+
+//What should your API documentation describe?
+//JSON
+//HTML
+//common use casses answer
+//your tech stack
+
+//The type of operation requested is determined by?
+
+//http PaymentMethod answer
+//http status Code
+//resouce path
+//query string
+
+//What metric tracks overall availability for your API
+//response time
+///time to first hello world
+//ttl
+//uptime anwer
+
+//Which is a common command-line tool for using or exploring an API?
+//bash
+//curl answer
+//powershell
+//ssh
+
+//What is the most secure method to transmit an Api key?
+//URL paramter
+//authorization header anser
+//base64 encoding
+//basic auth
+
