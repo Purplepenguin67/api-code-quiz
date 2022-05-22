e53d357ebd38581b46bd121d0040aff8a2251f6f
Douglas Snodgrass <douglassnodgrass@Mac-mini.lan>
2022-05-21 20:11:35 -0700

    final changes

---
+++ js/questions.js
@@ -0,0 +1,59 @@
+// Question Array
+let questions = [
+  {
+  numb: 1,
+  question: "API traffic that is entilry internal to you organization is normally called?",
+  answer: "internal traffic",
+  options: [
+    "inbound traffic",
+    "north-south traffic",
+    "internal traffic",
+    "east-wet traffic"
+  ]
+},
+  {
+  numb: 2,
+  question: "What should your API documentation describe?",
+  answer: "common use cases",
+  options: [
+    "JSON",
+    "HTML",
+    "common use cases",
+    "your tech stack"
+  ]
+},
+  {
+  numb: 3,
+  question: "The type of operation requested is determined by?",
+  answer: "http payment method",
+  options: [
+    "http payment method",
+    "http status code",
+    "resource path",
+    "query string"
+  ]
+},
+  {
+  numb: 4,
+  question: "What metric tracks overall availability for your API?",
+  answer: "uptime",
+  options: [
+    "response time",
+    "time to first hello world",
+    "ttl",
+    "uptime"
+  ]
+},
+  {
+  numb: 5,
+  question: "Which is a common command-line tool for using or exploring an API",
+  answer: "Curl",
+  options: [
+    "Curl",
+    "bash",
+    "powershell",
+    "ssh"
+  ]
+},
+
+];
